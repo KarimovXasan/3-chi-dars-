1fceb8a187e388406cab1a666e76ba71bb01e30c
Xasan Karimov <xkarimov257@gmail.com>
2023-11-30 17:02:59 +0500

    dfdf

---
+++ main.py
@@ -0,0 +1,46 @@
+print('Hello  World')
+
+
+yosh = int(input("Yoshingizni kiriting: "))  #bu 2- misolni 1-si
+
+if yosh <5 or yosh>59:
+    print("Chiptalar tekin")
+
+else:
+    print("Chiptalarga pul tolashingiz kerak")
+
+
+# 1-misol
+
+son = int(input("Juft raqam kiriting"))
+
+if son % 2 ==0:
+    print("To'g'ri")
+
+else:
+    print("Bu juft raqam emas")
+
+d= int(input("yoshingizni kiriting:")
+if d<5 or d>59:
+ print("Chipta tekin")
+elif d< 17:
+    print("chipta narxi 10.000")
+elif d>18:
+    print("chipta 20.000")
+else:
+    print(".")
+
+# 3-misol
+
+
+
+x = int(input("raqam kiriting: "))
+keyingi_raqam = x+1
+print(f"{x} dan keyin {keyingi_raqam} keladi")
+
+
+
+
+
+
+
