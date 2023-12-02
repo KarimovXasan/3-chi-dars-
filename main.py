223aa08078fd5d92c3c58f71e02a8ead83afba75
Xasan Karimov <xkarimov257@gmail.com>
2023-12-02 15:45:52 +0500

    first

--- main.py
+++ main.py
@@ -1,42 +1,13 @@
-print('Hello  World')
-
-
-yosh = int(input("Yoshingizni kiriting: "))  #bu 2- misolni 1-si
-
-if yosh <5 or yosh>59:
-    print("Chiptalar tekin")
-
+a = 266
+b = int(input("2 dan 8 gacha kiriting"))
+if a % b:
+    print("notogri")
 else:
-    print("Chiptalarga pul tolashingiz kerak")
+    print("togri")
 
-
-# 1-misol
-
-son = int(input("Juft raqam kiriting"))
-
-if son % 2 ==0:
-    print("To'g'ri")
-
-else:
-    print("Bu juft raqam emas")
-
-d= int(input("yoshingizni kiriting:")
-if d<5 or d>59:
- print("Chipta tekin")
-elif d< 17:
-    print("chipta narxi 10.000")
-elif d>18:
-    print("chipta 20.000")
-else:
-    print(".")
-
-# 3-misol
-
-
-
-x = int(input("raqam kiriting: "))
-keyingi_raqam = x+1
-print(f"{x} dan keyin {keyingi_raqam} keladi")
+a = int(input("telefonlar soni:"))
+b = int(input("odam soni"))
+print(a - b)
 
 
 
