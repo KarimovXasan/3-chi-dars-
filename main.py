ce9e06af276db16774c959ebc2486977572d996d
Xasan Karimov <xkarimov257@gmail.com>
2023-12-19 16:45:04 +0500

    xsa

--- main.py
+++ main.py
@@ -1,28 +1,83 @@
-number = [[1,2,3,],[8,9,10,11,12]]
-
-for x in number:
-    for y in x:
-        print(y)
-
-
-car = {
-    'Damas': {
-        "made_in": "Uzbekistan",
-        "prise":"10000$",
-        "color":"white-black"
-    },
-
-    'BMW':{
-        "made_in":"Germaniya",
-        "price":"10000$",
-        "color":"blue-yellow-red"
-    }
+a = input("Enter the first food: ")              # 1-misol
+b = input("Enter the second food: ")
+d = input("Enter the third food: ")
+def menu():
+menu = {
+    "Osh": "20000",
+    "Manti": "30000",
+    "Non": "3000",
+    "SHo'rva": "25000",
+    "Norin": "35000",
+    "Kabob": "25000",
+    "Mastava": "20000"
 }
 
-for key, value in car.items():
-    for _,x in value
+user_dishes = 3
+
+for food in a,b,d:
+    if food in menu:
+        print(f"The price of {food} is ${menu[food]}.")
+    else:
+        print("We do not have such a food.")
+menu()
 
 
-    .items()
-      print(x)
 
+
+b = input("Please enter a country: ") # 2-misol
+def menu():
+a = {
+    "O'ZBEKISTON": 'Tashkent',
+    'AQSH': 'Washington D.C.',
+    'BRITANYA': 'London',
+    "FRANSIYA": 'Paris',
+    'GERMANIYA': 'Berlin',
+    'KANADA': 'Ottava',
+    'AUTRALIA': 'Kanberra',
+}
+
+if b in a:
+    print(f"The capital of {b} is {a[b]}.")
+else:
+    print("Error")
+menu()
+
+
+
+
+# 3-misol
+def menu():
+dictionary =  {
+    "Issubset -": "Agar a obyektdagi elementlar b obyektrida bo’lsa True , bo’lmasa False.",
+    "Float -": "O'nik son",
+    "For -": "Biror amalni qayta qaytabajarish tsikli",
+    "Integer -": "Butun son",
+    "Boolean -": "mantiqiy qiymat",
+    "Append -": "Elementlarni oxiriga tushuradigan method",
+    "Insert -": "Elementlarni index orqali hoxlagan joyiga tushuradi",
+    "Range -": "Faqat integer bilan ishlaydi",
+    "Delete -": "Elementlarni index orqali o'chiradi",
+    "Print -": "Konsolga chiqari berish uchun ishlatiladi"
+}
+for key, value in sorted(dictionary.items()):
+    print(key, value)
+menu()
+
+
+
+# 4-misol
+def menu():
+    countries = {
+        "AQSH ": "WASHINGTON",
+        "ITALIYA ": "RIM",
+        "MALAYZIYA ": "SINGAPUR",
+        "O'ZBEKISTON ": " ASHKENT",
+        "QIRG'IZISTON ": "BISHKEK",
+        "ROSSIYA ": "MOSKVA",
+        "SINGAPUR": "KUALA-LUMPUR",
+        "TOJIKISTON": "DUSHENBE"
+    }
+
+    for key, value in sorted(countries.items()):
+        print(key, value)
+menu()
